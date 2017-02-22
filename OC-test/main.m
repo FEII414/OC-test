bd4f47d9be40e94907c452e7dfba984be27d57bb
feii <feii@feiidemacbook-air-4.local>
2017-02-22 14:17:52 +0800

    Initial Commit

---
+++ OC-test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC-test
+//
+//  Created by feii on 17/2/22.
+//  Copyright © 2017年 feii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
